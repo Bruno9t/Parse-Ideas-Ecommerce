234c8dbb60fb62f3e204f20b0205233e0401f24e
Bruno9t <bru_acad@outlook.com>
2020-05-09 14:03:48 -0300

    initial setup

---
+++ config/databaseConfig.js
@@ -0,0 +1,11 @@
+// aqui vem as informações para conexão com seu banco de dados
+
+module.exports = {
+
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "",
+
+  }
