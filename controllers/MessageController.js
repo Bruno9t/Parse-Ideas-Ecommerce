8f8a35bb892671f8deeb964ef6742b0d872a10f5
brulazzaro <brulazzaro@gmail.com>
2020-06-02 14:32:23 -0300

    almost message send finished

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,8 +1,13 @@
 const {Message} = require('../models');
 
 const MessageController = {
-  create:(req,res)=>{
+  create: async (req,res)=>{
+    let {usuario_id, anuncio_id, nome, telefone, mensagem} = req.body
 
+    let ok = await Message.create({usuario_id, anuncio_id, nome, telefone, mensagem})
+
+    console.log(ok)
+    res.json(ok)
   }
 }
 
