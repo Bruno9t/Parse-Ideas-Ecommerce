215177faa137b4cf335c303193c277238fda8f44
brulazzaro <brulazzaro@gmail.com>
2020-06-02 22:46:10 -0300

    create message

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,13 +1,23 @@
 const {Message} = require('../models');
 
 const MessageController = {
-  create: async (req,res)=>{
-    let {usuario_id, anuncio_id, nome, telefone, mensagem} = req.body
-
-    let ok = await Message.create({usuario_id, anuncio_id, nome, telefone, mensagem})
-
-    console.log(ok)
-    res.json(ok)
+  create: (req,res)=>{
+    let {usuario_id, anuncio_id, nome, email, celular, telefone, mensagem} = req.body
+    
+    return Message.create({
+      usuario_id:usuario_id,
+      anuncio_id:anuncio_id,
+      nome:nome,
+      email:email,
+      celular:celular,
+      telefone:telefone,
+      mensagem:mensagem
+    }).then(ok => {
+      return res.json({resp:'ok'})
+    }).catch(err => {
+      console.log(err)
+      return res.json({resp:'error'})
+    })
   }
 }
 
