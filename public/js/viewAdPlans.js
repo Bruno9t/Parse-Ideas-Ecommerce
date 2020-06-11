e63a922c0a8ebcac9eab500fd121f9fb22bb2863
carolinacabril <carolina-cabril@hotmail.com>
2020-06-11 02:47:41 -0300

    create viewAdPlans.js

---
+++ public/js/viewAdPlans.js
@@ -0,0 +1,5 @@
+ let href = document.querySelector('#btn-submit')
+    href.setAttribute('href', window.location.href+'/list')
+    
+let button = document.querySelector('#button')
+    button.setAttribute('href', window.location.href+'/list')
