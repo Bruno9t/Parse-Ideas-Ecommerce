6b3800b85e954d5c4cb1414bbdd94d209a5cdfdf
Bruno9t <bru_acad@outlook.com>
2020-06-12 12:48:23 -0300

    alter app.js

--- routes/plans.js
+++ routes/plans.js
@@ -4,8 +4,8 @@ const PlanController = require('../controllers/PlanController');
 
 router.get('/plans', PlanController.index);
 router.get('/plans/admin/create', PlanController.create);
-router.get('/admin/list', PlanController.list);
-router.get('/admin/active/:id', PlanController.active);
+router.get('/plans/admin/list', PlanController.list);
+router.get('/plans/admin/active/:id', PlanController.active);
 
 router.get('/plans/singForm', PlanController.sign);
 router.post('/plans/signPlan', PlanController.signPlan);
