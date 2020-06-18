cb599fbffd23ea22b04a3c0d8735678a58c34ffd
Bruno9t <bru_acad@outlook.com>
2020-06-18 20:45:45 -0300

    set header of payment page

--- public/js/alterActive.js
+++ public/js/alterActive.js
@@ -2,12 +2,17 @@ let ul = document.querySelector('div#mainMenu ul')
 let lastClicked = document.querySelectorAll('li.nav-item')
 let imgHome = document.getElementById('image-home')
 
-lastClicked[localStorage.getItem('lastClicked')].classList.add('active')
+lastClicked[localStorage.getItem('lastClicked') || 1].classList.add('active')
 
 ul.addEventListener('click',function(e){
     localStorage.setItem('lastClicked',lastClicked[e.target.parentElement.id].id)
     alterActive(e.target)
 
+})
+
+imgHome.addEventListener('click',function(){
+    localStorage.setItem('lastClicked','1')
+    alterActive(e.target)
 })
 
 console.log(localStorage.getItem('lastClicked'))
